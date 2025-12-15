894498ef918b000d2c1d22fd0486198b056cfdce
Saleem Beig <Saleem.Beig@hotmail.com>
2025-12-15 15:15:37 +0100

    initial commit

---
+++ AlgorithmicQuestions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+   public static  class AlgorithmicQuestions
+    {
+       public static string ReverseString(string input)
+        {
+            char[] chars = input.ToCharArray();
+            int left = 0, right = chars.Length - 1;
+
+            while (left < right)
+            {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+
+            return new string(chars);
+        }
+       public static bool IsPalindrome(string s)
+        {
+            int left = 0, right = s.Length - 1;
+
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                    return false;
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Practice;
+
+Console.WriteLine("Enter a String");
+var input= Console.ReadLine();
+
+//Write console write to take user choice
+
+var result=AlgorithmicQuestions.ReverseString(input); ;
+Console.WriteLine($"Result {result}");
+Console.ReadKey();
